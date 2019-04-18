da186387a37f7cca0205f665c5d1d9774c9c39c6
Somat Thakali <sthakal000@citymail.cuny.edu>
2019-04-18 15:24:06 -0400

    Changed the name od id

--- db/schema.sql
+++ db/schema.sql
@@ -9,9 +9,9 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-    burger_id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(200) NOT NULL,
     devoured BOOLEAN DEFAULT false,
-    PRIMARY KEY (burger_id)
+    PRIMARY KEY (id)
 
 )
