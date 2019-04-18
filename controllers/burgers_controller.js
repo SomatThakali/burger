9b21746c3817cce6a448ad07c5f418a74254c3c2
Somat Thakali <sthakal000@citymail.cuny.edu>
2019-04-18 15:48:26 -0400

    Removing spaces

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -26,7 +26,7 @@ router.post("/api/burgers", (req, res) => {
 });
 
 router.put("/api/burgers/:id", (req, res) => {
-  let condition = "id = " + req.params.id;
+  let condition = `id = ${req.params.id}`;
   console.log(req.params.id);
   console.log("condition", condition);
 
